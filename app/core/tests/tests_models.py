8908f7e5129e3fb1d2a22372bdd9b80859f1f375
Maciej Jarocki <mmaciej.jarocki@gmail.com>
2021-10-17 17:44:42 +0200

    Added create tags feature and endpoint for list tags

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@gmail.com', password='password'):
+    """Create a simple user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTest(TestCase):
 
@@ -30,3 +37,11 @@ class ModelTest(TestCase):
         user = get_user_model().objects.create_superuser('testsmj@gmail.com', 'password123')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_the_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
